762ac3da92d8c66e5ff22f595c782ec91ae67fe8
KamilW04 <s41747@s.pansjar.edu.pl>
2026-05-20 11:16:58 +0200

    R2-7 Przygotowanie wyglądu menu
    R2-12 Przygotowanie wyglądu zarządzania menu

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('menu');
 });
+
+Route::get('/manager', function ()
+{
+    return view('menu-manager');
+})->name('manager.podglad');
