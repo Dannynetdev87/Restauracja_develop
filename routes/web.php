cd28eb35708b7f2d89ce556563e2bc7e096a8c6c
Yuqio552 <s41745@s.pwste.edu.pl>
2026-05-20 11:49:13 +0200

    Layout + wygląd zarządzania stolikami

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Ta trasa zostaje – to ona wyświetla naszą nową stronę główną restauracji!
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+// Tutaj w przyszłości dopiszemy kolejne adresy, np.:
+// Route::get('/menu', [MenuController::class, 'index']);
+// Route::get('/rezerwacje', [ReservationController::class, 'create']);
